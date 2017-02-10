be4e7cbdc651566f28bb600c0bab0f8a71642dec
Benis Tambe <will.jenifer@yahoo.com>
2017-02-09 19:39:26 -0500

    My first Commit for MEAN

---
+++ BenisT/test.py
@@ -0,0 +1,13 @@
+# def Score_and_grade(grade):
+#     if(grade > 59 and grade <101):
+#         if(grade > 59 and grade < 70):
+#             print('you got a D')
+#         else:
+#             print(' Your grade is yet to be determined')
+#     else:
+#         print('Grade is not in range')
+#     for number in range(1, 11):
+#         retrived_grade = raw_input("What is your grade?")
+#         Score_and_grade(retrived_grade)
+# retrived_grade = raw_input("What is your grade?")
+# Score_and_grade(retrived_grade)
